7ce36fc811dd0cce9d4b637d38dfd0a93ccd3a6b
CMLTTN <cemaldim@gmail.com>
2023-03-24 06:45:54 +0300

    url

--- MY_STOCK_APP/src/apiCall/authCall.jsx
+++ MY_STOCK_APP/src/apiCall/authCall.jsx
@@ -4,7 +4,7 @@ export const login  = async (userInfo)=>{
      const  BASE_URL ="https://12255.fullstack.clarusway.com/redoc/"
 
 try{
-     const {data} = await axios.post(url,userInfo)  
+     const {data} = await axios.post(`${BASE_URL}account/auth/login/`,userInfo)  
 } catch (error) {
      console.log(error);
 }
