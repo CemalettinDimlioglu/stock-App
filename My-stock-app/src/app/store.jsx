230a2d3bae5fc2d31ed486c37369524a7c720689
CMLTTN <cemaldim@gmail.com>
2023-03-28 09:04:18 +0300

    store

--- My-stock-app/src/app/store.jsx
+++ My-stock-app/src/app/store.jsx
@@ -1,7 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit"
 import authReducer from "../features/authSlice"
 import stockReducer from "../features/stockSlice"
-
 import { persistStore, persistReducer } from "redux-persist"
 import storage from "redux-persist/lib/storage" // defaults to localStorage for web
 
