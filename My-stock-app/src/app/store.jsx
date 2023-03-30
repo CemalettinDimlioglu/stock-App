d50a21ee4fa5c67e819604f1095058d9981db08b
CMLTTN <cemaldim@gmail.com>
2023-03-30 09:05:42 +0300

    middleware

--- My-stock-app/src/app/store.jsx
+++ My-stock-app/src/app/store.jsx
@@ -1,23 +1,38 @@
-import { configureStore } from "@reduxjs/toolkit"
-import authReducer from "../features/authSlice"
-import stockReducer from "../features/stockSlice"
-import { persistStore, persistReducer } from "redux-persist"
-import storage from "redux-persist/lib/storage" // defaults to localStorage for web
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "../features/authSlice";
+import stockReducer from "../features/stockSlice";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 const persistConfig = {
   key: "root",
   storage,
-}
+};
 
-const persistedReducer = persistReducer(persistConfig, authReducer)
+const persistedReducer = persistReducer(persistConfig, authReducer);
 
 const store = configureStore({
   reducer: {
     auth: persistedReducer,
     stock: stockReducer,
   },
+  middleware: (getDefaultMiddleWare) =>
+    getDefaultMiddleWare({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
   devTools: process.env.NODE_ENV !== "production",
-})
+});
 
-export const persistor = persistStore(store)
-export default store
+export const persistor = persistStore(store);
+export default store;
