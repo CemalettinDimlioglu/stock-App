741f32bd4dbc7fb977701722a3c354636d0f5441
CMLTTN <cemaldim@gmail.com>
2023-03-31 09:21:47 +0300

    firms

--- My-stock-app/src/pages/Firms.jsx
+++ My-stock-app/src/pages/Firms.jsx
@@ -1,9 +1,16 @@
-
+import { Button } from "@mui/material";
+import Typography from "@mui/material/Typography";
 
 const Firms = () => {
   return (
-    <div>Firms</div>
-  )
-}
+    <div>
+      <Typography variant="h4" color="error" mb={3}>
+        Firm
+      </Typography>
+      <Button variant="contained" >New Firm</Button>
+      
+    </div>
+  );
+};
 
-export default Firms
+export default Firms;
