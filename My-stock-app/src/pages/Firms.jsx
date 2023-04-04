151ce1be4e11352bb043caecd8cbdb7e59a929f8
CMLTTN <cemaldim@gmail.com>
2023-04-04 13:19:07 +0300

    films

--- My-stock-app/src/pages/Firms.jsx
+++ My-stock-app/src/pages/Firms.jsx
@@ -1,7 +1,38 @@
 import { Button } from "@mui/material";
 import Typography from "@mui/material/Typography";
+import { useEffect } from "react";
+import axios from "axios"
+import { useDispatch, useSelector } from "react-redux";
+import {fetchFail, getSuccess, fetchStart} from "../features/stockSlice"
 
 const Firms = () => {
+  const {token} = useSelector(state=>state.auth)
+  const dispatch = useDispatch()
+  const getFirms = async ()=>{
+
+  const BASE_URL = " https://12255.fullstack.clarusway.com/"
+
+  dispatch(fetchStart())
+  const url = "firms"
+  try {
+
+  const {data} = await axios (`${BASE_URL}stock/firms/`, {
+    headers:{Authorization: `Token ${token}`}
+  })
+  dispatch(getSuccess({data , url }))
+  
+  } catch (error) {
+   console.log(error); 
+   dispatch(fetchFail())
+  }
+
+  
+  }
+  useEffect(() => {
+    getFirms()
+  
+  }, [])
+  
   return (
     <div>
       <Typography variant="h4" color="error" mb={3}>
